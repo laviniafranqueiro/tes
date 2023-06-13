b801a26bba0bdd3076aa99d151e843be1a6413a3
laviniafranqueiro <133999323+laviniafranqueiro@users.noreply.github.com>
2023-06-13 09:11:46 -0300

    Update apresentacao.py

--- apresentacao.py
+++ apresentacao.py
@@ -4,7 +4,14 @@ import pandas as pd
 st.title('avaliação do Josir')
 st.caption('Lavínia Franqueiro')
 
+user_input = st.text_input("Digite a marca do notebook que você está procurando:")
+
 df = pd.read_csv('jupyter2.csv')  # Assuming 'jupyter2.txt' is your file name
+
+# Check if user has entered something in the text input box
+if user_input:
+    # Filter the DataFrame based on the user input
+    df = df[df['marca'].str.contains(user_input, case=False)]
 
 for i in range(df.shape[0]):
     marca = df.iloc[i]['marca']
