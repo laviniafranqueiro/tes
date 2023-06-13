97c6fd3030c3b4619d00f8c2bed93442c9f22d82
laviniafranqueiro <133999323+laviniafranqueiro@users.noreply.github.com>
2023-06-13 08:28:50 -0300

    Update A2.py

--- A2.py
+++ A2.py
@@ -6,4 +6,4 @@ st.caption('Lavínia Franqueiro')
 
 df = pd.read_csv('jupyter2', sep =',')
 st.dataframe(df)
-st.write("Click [here](http://www.example.com) to visit the website.")
+st.write("Click [here](https://www.kabum.com.br) to visit the website.")
